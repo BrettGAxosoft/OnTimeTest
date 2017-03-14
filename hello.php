e0406f708d2ffbb896745e38649bfa199d34d9ee
Brett Goldman <brettg@axosoft.com>
2017-03-14 15:37:35 -0700

    test with branch from master [axof: 1 wl: 2 hrs]

--- hello.php
+++ hello.php
@@ -3,6 +3,6 @@
   <title>PHP Test</title>
  </head>
  <body>
- <?php echo '<p>Hello from Rigel 7 I'm Kodos.</p>'; ?> 
+ <?php echo '<p>Hello from Rigel 7 I'm Kodos. Abortions for Some!</p>'; ?> 
  </body>
 </html>
